43e1d06f6607dd7bad130953e5f3b91e30b1314c
Akkaiah-pro <ita.paleru@gmail.com>
2025-07-16 13:02:13 +0530

    Updates signin class

--- signin.java
+++ signin.java
@@ -3,5 +3,7 @@ public class Signin
   public static void main(String[] args)
   {
     System.out.println("Signin");
+    System.out.println("Username.");
+    System.out.println("password.");
   }
 }
